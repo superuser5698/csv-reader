093a48c9dc0075d5010a09495e1fd7bbe9bc8b18
AyaanshRobotics <superuser5698@outlook.com>
2026-02-24 18:48:46 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-print("welcome to online job tracker :)")
-import subprocess
-import read, write
-
 try:  
-    import pandas as pd #type: ignore
+    import pandas as csv_reader #type: ignore
 except ImportError:
     print("pandas not installed installing...")
     result =  subprocess.run(["pip", "install", "pandas"])
    # print(result)
-def main():
-    personnum = 0
-    while True:
+def write(data):
+    import pandas as write_csv
+    writer = write_csv.DataFrame(data)
+    writer.to_csv('data.csv', mode="a", index=False)
+import pandas as csv_reader
+
+def read():
+    file = open("data.csv", "r", -1, "utf-8")
+    file.close()
+    try:
+        csv_file = csv_reader.read_csv('data.csv')
+    except:
+        return     
+    return csv_file.head()
+
+print("welcome to online job tracker :")
+import subprocess
+with open("data.csv",mode="a" ) as f:
+    print("checked that database exsisted")
+    f.close()
+
+
+
+personnum = 0
+while True:
         
         option = input("read or write, q to quit: ")
  #       print(option)
         if option == "read":
-            print(read.read())
+            print(read())
         elif option == "write":
             write_stuff_name = input("who's name would you like to add to comapny list: ")
             write_stuff_jobs = input(f"{write_stuff_name}'s job: ")
             semifianl_stuff = write_stuff_name + "'s job is " + write_stuff_jobs
             computer_langouage_write_stuff = {personnum: [semifianl_stuff]}
-            write.write(computer_langouage_write_stuff)
+            write(computer_langouage_write_stuff)
             personnum = personnum + 1
         elif option =="q":
             break
         else:
             print("I don't understand deal with it I'm a job tracker")
-
-
-main()
